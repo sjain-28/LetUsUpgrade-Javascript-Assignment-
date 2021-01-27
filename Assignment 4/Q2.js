d839107c12b5b9878f036dfb856d92a8c7a173c4
Shubham Jain <76805799+sjain-28@users.noreply.github.com>
2021-01-27 03:02:02 -0800

    Assignment 4 submitted

---
+++ Assignment 4/Q2.js
@@ -0,0 +1,8 @@
+
+
+function add(){
+  var n1 = Number(document.getElementById("n1").value);
+  var n2 = Number(document.getElementById("n2").value);
+  var add = n1 + n2;
+  document.getElementById("result").value = add;
+}
